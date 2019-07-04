a1e2db00e450b6e994be3d3f5f59ba2c11c053a6
Andrew Silva <andrewsilva9@gmail.com>
2019-07-04 01:54:06 +0000

    pry in scrape file

--- lib/top_30_ski_resorts/scrape_website.rb
+++ lib/top_30_ski_resorts/scrape_website.rb
@@ -4,7 +4,10 @@ require 'pry'
 def resort_hash
   
   file = File.read('https://www.zrankings.com/')
+  zrankings = Nokogiri::HTML(file)
   resorts = {}
   
+  zrankings.css(".tableshow table-wrap indie-blue").each do |resort|
+    binding.pry
 end
   
