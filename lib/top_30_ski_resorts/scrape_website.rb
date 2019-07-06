7593e93d17fd54d9d9df3a21c261b66e1c0264e8
Andrew Silva <andrewsilva9@gmail.com>
2019-07-06 16:54:23 +0000

    changes to Scraper class

--- lib/top_30_ski_resorts/scrape_website.rb
+++ lib/top_30_ski_resorts/scrape_website.rb
@@ -1,14 +1,16 @@
 require 'nokogiri'
 require 'pry'
+require 'open-uri'
 
 class Scraper
  def resort_hash
   
    file = File.read('https://www.zrankings.com/')
    zrankings = Nokogiri::HTML(file)
-   resorts = {}
+   resorts = []
   
    zrankings.css(".tableshow table-wrap indie-blue").each do |resort|
+     resort_details = {}
     binding.pry
  end
   
