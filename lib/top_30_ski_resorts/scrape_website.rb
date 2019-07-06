13ef8f1ac599fb16892be3841e14a02886988621
Andrew Silva <andrewsilva9@gmail.com>
2019-07-06 16:34:54 +0000

    changed notes in resort_cli

--- lib/top_30_ski_resorts/scrape_website.rb
+++ lib/top_30_ski_resorts/scrape_website.rb
@@ -1,13 +1,14 @@
 require 'nokogiri'
 require 'pry'
 
-def resort_hash
+class Scraper
+ def resort_hash
   
-  file = File.read('https://www.zrankings.com/')
-  zrankings = Nokogiri::HTML(file)
-  resorts = {}
+   file = File.read('https://www.zrankings.com/')
+   zrankings = Nokogiri::HTML(file)
+   resorts = {}
   
-  zrankings.css(".tableshow table-wrap indie-blue").each do |resort|
+   zrankings.css(".tableshow table-wrap indie-blue").each do |resort|
     binding.pry
-end
+ end
   
