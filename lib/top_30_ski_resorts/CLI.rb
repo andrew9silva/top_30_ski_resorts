e07b697b28605ee4c0be298d1a945e7730ac6535
Andrew Silva <andrewsilva9@gmail.com>
2019-07-06 20:53:31 +0000

    started case statement for ski resorts

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -2,11 +2,24 @@ class Top30SkiResorts::CLI
   
   def resorts
     puts "Are you going on a ski trip this year? (Y/N)"
-    states_array
+    
+    if "Y"
+      states
+    else
+      "Aww man that's no fun!"
+    end
   end
   
-  def states_array
+  def states
     puts "Where do you want to ski? California, Canada, Colorado, Montana, New Mexico, Utah, Wyoming."
+    state = gets.strip
+    
+    case state
+    when "California"
+      puts "california_array"
+    when "Canada"
+      puts "canada_array"
+    end
   end
     
 end
