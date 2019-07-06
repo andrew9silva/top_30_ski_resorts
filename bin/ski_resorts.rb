16fe49455d5e220187af2c9e9f725ff8c85c5090
Andrew Silva <andrewsilva9@gmail.com>
2019-07-06 18:12:43 +0000

    added requires

--- bin/ski_resorts.rb
+++ bin/ski_resorts.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 
+require './lib/top_30_ski_resorts'
+
 Top30SkiResorts::CLI.new.resorts
--- lib/top_30_ski_resorts.rb
+++ lib/top_30_ski_resorts.rb
@@ -1,6 +1,8 @@
-require "top_30_ski_resorts/version"
+require_relative "./top_30_ski_resorts/version"
 
 module Top30SkiResorts
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative './top_30_ski_resorts/CLI'
