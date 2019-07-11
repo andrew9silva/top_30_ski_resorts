3aeda3facb92d263676cea76308105cb648e1ad7
Andrew Silva <andrewsilva9@gmail.com>
2019-07-11 02:01:10 +0000

    improving scraping in resorts.rb

--- lib/top_30_ski_resorts/resorts.rb
+++ lib/top_30_ski_resorts/resorts.rb
@@ -4,22 +4,23 @@ require 'open-uri'
 
 class Top30SkiResorts::Resorts
   
+  attr_accessor :state, :full_report
+  
   #want to scrape and return hash that has each resort as a :key and the 'Full Report' link as the 'value' in each state's method.  example for Colorado.... "{Telluride Ski Resort" => "/ski-resorts/163-telluride-ski-resort, "Vail Ski Resort" => "/ski-resorts/169-vail-ski-resort"... etc.
   
   
   
   
-  
+  self.full_report = document.search("a#btn-rank-index.btn-more-snow-index.more-profile").first.attr("href")
   
   
   def self.colorado
     
-    array = []
+    colorado_resorts = []
     
     document = Nokogiri::HTML(open('https://www.zrankings.com/'))
     
-    document.css("table.index-table-2017").each do |resort|
-      #report = document.search("a#btn-rank-index.btn-more-snow-index.more-profile").first.attr("href")
+      #full_report = document.search("a#btn-rank-index.btn-more-snow-index.more-profile").first.attr("href")
       binding.pry
     end
     
