cd511b808717944086a8b39ca8be2521d502bd3a
Andrew Silva <andrewsilva9@gmail.com>
2019-07-24 02:14:14 +0000

    working on scraper class

--- lib/top_30_ski_resorts/scraper.rb
+++ lib/top_30_ski_resorts/scraper.rb
@@ -7,33 +7,19 @@ class Top30SkiResorts::Scraper
     document = Nokogiri::HTML(open('https://www.zrankings.com/'))
     
     
-    #mountain.css("a")[1].attributes["href"].value = resort link?
-    #mountain.css("span.desktop-700").children.text = resort state?
+    #mountain.css("a")[1].attributes["href"].value = full_report
+    #mountain.css("span.desktop-700").children.text = state
     
-    document.css("tbody.single-resort-cell span.desktop-700").each do |mountain|
+    document.css("table.index-table-2017 tbody.single-resort-cell").each do |mountain|
       
   end 
 end
-  
-  # def self.scrape_resort_cell
-  #   document = Nokogiri::HTML(open('https://www.zrankings.com/'))
-  #   reports = {}
-    
-  #   #mountain.css("a")[1].attributes["href"].value = resort link?
-  #   #mountain.css("span.desktop-700").children.text = resort state?
-    
-  #   document.css("tbody.single-resort-cell span.desktop-700").each do |mountain|
-      
-  # end 
   
   #want to return array with link to each resort in particular state in the top 30.
   
   
   
-  #full_report = document.search("td.name-rank")[0].children[0].attr("href")
-  #state = document.search("td.name-rank")[0].children.children[2].text
-  #document = Nokogiri::HTML(open('https://www.zrankings.com/'))
-  #document.css("table.index-table-2017").each do |mountain|
+  
   
    
   
