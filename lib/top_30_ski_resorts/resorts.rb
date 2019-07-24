a8fe0a4396a82db6dfcacfed93eaa585920214ca
Andrew Silva <andrewsilva9@gmail.com>
2019-07-24 01:22:42 +0000

    workin on initialize method

--- lib/top_30_ski_resorts/resorts.rb
+++ lib/top_30_ski_resorts/resorts.rb
@@ -1,9 +1,11 @@
 class Top30SkiResorts::Resorts 
   
-  attr_accessor :ski_resort
+  attr_accessor :state :full_report 
   
-  def initialize(ski_resort)
-    @ski_resort = ski_resort
-  endxs
+  def initialize(state, full_report)
+    @state = state
+    @full_report = full_report
+    
+  end
   
 end
