c782533e19f43ac0ded8ef0d4d6afbcc69d79f7b
Andrew Silva <andrewsilva9@gmail.com>
2019-07-27 01:52:03 +0000

    further work on CLI

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -1,8 +1,19 @@
 class Top30SkiResorts::CLI
   
  def resorts
-  puts "Where will you be skiing this year?"
-  puts Top30SkiResorts::Scraper.scrape_resorts
+   
+   puts "Are you going on a ski trip this year? (Yes or No)"
+   input = gets.strip
+   if input == "Yes"
+    puts Top30SkiResorts::Scraper.scrape_resorts
+  
+   elsif input == "No"
+    puts "That's lame"
+   
+   else
+    puts "Not sure what you mean?"
+    resorts
+   end
  end
   
   
