c171336d4c291d1d5937d1f0c6048c2001f5c354
Andrew Silva <andrewsilva9@gmail.com>
2019-07-27 00:52:18 +0000

    starting a test CLI

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -1,5 +1,11 @@
 class Top30SkiResorts::CLI
   
+ def greeting
+  puts "Where will you be skiing this year?"
+  Top30SkiResorts::Scraper.scrape_resorts
+ end
+  
+  
   
 end
   
--- lib/top_30_ski_resorts/scraper.rb
+++ lib/top_30_ski_resorts/scraper.rb
@@ -4,14 +4,17 @@ require 'open-uri'
 
 class Top30SkiResorts::Scraper
   
-  def self.scrape_resorts(state)
+  @@all = []
+  
+  def self.scrape_resorts
     document = Nokogiri::HTML(open('https://www.zrankings.com/'))
   
     document.css("table.index-table-2017 tbody.single-resort-cell").each do |mountain|
       
       name = mountain.children[3].children[3].children[0].text
+      state = mountain.children[3].children[3].children[5].children.text
       full_report = mountain.children.children[22].children[0].attr("href")
-      Top30SkiResorts::Mountain.new(name, state, full_report)
+      @@all << Top30SkiResorts::Mountain.new(name, state, full_report)
     end
   end 
   
