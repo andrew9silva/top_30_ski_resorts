aade152793b6e81bb4a38ce80992a7ee7fabdd66
Andrew Silva <andrewsilva9@gmail.com>
2019-08-06 00:50:32 +0000

    added self.create and add_resorts to States class

--- lib/top_30_ski_resorts/states.rb
+++ lib/top_30_ski_resorts/states.rb
@@ -1,14 +1,12 @@
 class Top30SkiResorts::States 
   
-  attr_accessor :state_name, :resorts 
+  attr_accessor :state_name 
   
   @@all_states = []
   
   def initialize(state_name)
     @state_name = state_name
     @resorts = []
-    @resorts << Top30SkiResorts::Mountain.all
-    add_state
   end
   
   def add_state
@@ -20,7 +18,28 @@ class Top30SkiResorts::States
     @@all_states
   end
   
-  def self.get_resorts
-    Top30SkiResorts::Scraper.scrape_resorts(self) if @resorts.empty?
+  def self.create(state_name)
+    self.new(state_name).tap do |state|
+      state.save
+    end
   end
+  
+  def resorts
+    @resorts
+  end 
+  
+  def add_resorts(resort)
+    if resort.state == nil 
+      resort.state = self
+    else 
+      nil
+    end
+    if @resorts.include?(resort)
+      nil 
+    else
+      @resorts << resort 
+    end
+    resort
+  end 
+    
 end
