4de191317470d27c8d245e10af05189b6b20c3d8
Andrew Silva <andrewsilva9@gmail.com>
2019-08-06 01:00:17 +0000

    added self.create and self.find_by_name to Mountain class

--- lib/top_30_ski_resorts/mountain.rb
+++ lib/top_30_ski_resorts/mountain.rb
@@ -8,21 +8,30 @@ class Top30SkiResorts::Mountain
     @name = name
     @state_name = state_name
     @full_report = full_report
-    add_resort
   end
     
   def add_resort
     @@all_resorts << self
-    @@all_resorts
   end
   
   def self.all
-    Top30SkiResorts::Scraper.scrape_resorts if @@all_resorts.empty?
     @@all_resorts
   end
   
-  def add_to_state
-    Top30SkiResorts::States.resorts << self unless Top30SkiResorts::States.resorts.include?(self)
+  def self.create(name)
+    self.new(name).tap do |mountain|
+      mountain.add_resort
+    end
   end 
+  
+  def self.find_by_name(name)
+    @@all_resorts.detect do |mountain|
+      if mountain.name == name
+        mountain
+      else
+        nil 
+      end
+    end
+  end
   
 end
