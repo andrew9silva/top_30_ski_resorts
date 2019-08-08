2f6528af20325727720fdee405c97a07c7ecf445
Andrew Silva <andrewsilva9@gmail.com>
2019-08-08 02:50:07 +0000

    Able to extract Full Report Link

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -46,7 +46,7 @@ class Top30SkiResorts::CLI
    input = gets.strip
    
    if Top30SkiResorts::States.all.map.each(&:resorts).uniq[0][0].select { |x| x.name == input }
-    puts 
+    puts Top30SkiResorts::States.all.map.each(&:resorts).uniq[0][0].collect { |x| x.full_report }
    else
     puts "That wasn't an option"
     resort_selection
