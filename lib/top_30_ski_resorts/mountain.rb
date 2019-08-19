275ecd3fcdcbb32d3ce16a92efe834e9e9b6149a
Andrew Silva <andrewsilva9@gmail.com>
2019-08-19 00:41:45 +0000

    narrowed down States.all to be only the states one time based out initilializing Mountain class!

--- lib/top_30_ski_resorts/mountain.rb
+++ lib/top_30_ski_resorts/mountain.rb
@@ -3,12 +3,14 @@ class Top30SkiResorts::Mountain
   attr_accessor :name, :state_name, :full_report
   
   @@all_resorts = []
+  @@states = []
   
   def initialize(name, state_name, full_report)
     @name = name
     @full_report = full_report
     @state = Top30SkiResorts::States.find_or_create_by(state_name)
     add_resort
+    add_states
   end
     
   def add_resort
@@ -19,5 +21,12 @@ class Top30SkiResorts::Mountain
     Top30SkiResorts::Scraper.scrape_resorts if @@all_resorts.empty?
     @@all_resorts
   end
-
+  
+  def add_states
+    @@states << @state
+  end 
+  
+  def self.states
+    @@states
+  end
 end
