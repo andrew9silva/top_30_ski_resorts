e5717d951dec42396863f7f0460293f80947bdb6
Andrew Silva <andrewsilva9@gmail.com>
2019-08-19 02:02:22 +0000

    working on CLI

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -2,7 +2,6 @@ class Top30SkiResorts::CLI
  def resorts
    
    puts "Do you want to ski one of the best resorts in North America? (Yes or No)".colorize(:green)
-   binding.pry
    input = gets.strip
    if input == "Yes"
      get_state
@@ -27,25 +26,21 @@ class Top30SkiResorts::CLI
  end 
  
  def get_state
+   Top30SkiResorts::Mountain.all
    Top30SkiResorts::States.all
  end 
  
  def choose_state
    puts "Which state will you be skiing in?".colorize(:green)
-   Top30SkiResorts::States.all.each do |state|
+   @chosen_state = Top30SkiResorts::States.all.each do |state|
      puts "#{state.state_name}".colorize(:light_blue)
    end
  end
- 
  def state_options
    input = gets.strip
-   if Top30SkiResorts::States.all.any? { |x| x.state_name == input }
-     puts "Pick the number of the resort you want more information on.".colorize(:green)
-     state_resorts = Top30SkiResorts::States.all.map.each(&:resorts).uniq[0][0].select! { |x| x.state_name == input }
-     state_resorts.each.with_index(1) { |x, i| 
-       puts "#{i}. #{x.name}".colorize(:light_blue) 
-       
-     }
+   
+   if input == @chosen_state.any?
+     puts "test"
    else
      puts "Sorry that's not an option".colorize(:green)
      state_options
