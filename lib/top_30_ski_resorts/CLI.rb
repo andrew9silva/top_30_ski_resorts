c0bfb1e2098f2344c2b82b3212a2dac2d141a3b2
Andrew Silva <andrewsilva9@gmail.com>
2019-09-06 23:11:53 +0000

    need to return only the full_report for the resort I want, not for the whole state object

--- lib/top_30_ski_resorts/CLI.rb
+++ lib/top_30_ski_resorts/CLI.rb
@@ -39,9 +39,9 @@ class Top30SkiResorts::CLI
  def state_options
    input = gets.strip
    @chosen_state = Top30SkiResorts::States.all.keep_if { |x| x.state_name == input }
-   
+
    if Top30SkiResorts::States.all.each { |x| x.state_name == input }
-     puts @chosen_state[0].resorts[0..-1].map(&:name).inspect
+     puts @chosen_state.map(&:resorts)[0].map(&:name).inspect
    else
      puts "Sorry that's not an option".colorize(:green)
      choose_state
@@ -52,7 +52,7 @@ class Top30SkiResorts::CLI
    puts "Which resort do you want more information on?"
    input = gets.strip
    if  @chosen_state[0].resorts[0..-1].map.each(&:name).include?(input)
-     puts "test"
+     puts @chosen_state.map(&:resorts)[0].map(&:full_report)
    else
      nil 
    end
